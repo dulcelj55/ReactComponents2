7c2785fe33935c9f1c6333b71719d8344afee694
Dulce Jaramillo <dulcejaramillo@dulces-MacBook-Pro.local>
2023-10-02 14:23:42 -0700

    created something different for component3

--- src/Components/UserProfileCard/index.jsx
+++ src/Components/UserProfileCard/index.jsx
@@ -4,41 +4,54 @@ const UserProfileCard = () => {
     const [userName, setUserName]= useState('')
     const [userImage, setUserImage]= useState('')
     const [userBio, setUserBio]= useState('')
-    const [profile, setProfile]= useState([{userName, userImage, userBio}])
-let nameValue;
-let imageValue;
-let bioValue;
-const formRender = () =>{
-    setUserName(nameValue)
-    setUserImage(imageValue)
-    setUserBio(bioValue)
-}
-const showInfo = () =>{
-    setProfile(
-    profile.map((person)=>{
-        <>
-        <p>User Name: {person[0]}</p>
-        <p>User Image: {person[1]}</p>
-        <p>User Bio: {person[2]}</p>
-        </>
-    }))
-}
+    const [userNewName, setUserNewName]= useState('')
+    const [userNewImage, setUserNewImage]= useState('')
+    const [userNewBio, setUserNewBio]= useState('')
+    const[isSubmitted, setIsSubmitted]= useState(false)
 
+
+    const profile= [userNewName, userNewImage, userNewBio]
+
+    const showInfo = (e) =>{
+    e.preventDefault()
+    setUserNewName(userName)
+    setUserNewImage(userImage)
+    setUserNewBio(userBio)
+    setIsSubmitted(true)                
+    }
+
+  
+   
+ const returnInputsOrTexts =()=>{
+        if (!isSubmitted){
+            return(
+                <>
+                <h3>Please Enter your Information</h3>
+            <form onSubmit={showInfo}>
+                <input value={userName} onChange={(e)=> setUserName(e.target.value)} placeholder= 'User Name'></input>
+                <input value={userImage}  onChange={(e)=> setUserImage(e.target.value)} placeholder= 'Use Image'></input>
+                <input value={userBio} onChange={(e)=> setUserBio(e.target.value)} placeholder='UserBio'></input>
+                <button >Submit</button>
+            </form>         
+            </>
+            )
+        }else{
+            return(
+            <>
+                <p>User Image: {profile[1]}</p>
+                <p>You're logged in as: {profile[0]}</p>
+                <p>User Bio: {profile[2]}</p>       
+            </>
+            )
+        }
+
+ }
 
   return (
     <>
     <div>UserProfileCard</div>
-    <h3>Please Enter your Information</h3>
-<form>
-<input value={nameValue} onSubmit={formRender} placeholder= 'User Name'></input>
-<input value={imageValue} onSubmit={formRender} placeholder= 'Use Image'></input>
-<input value={bioValue} onSubmit={formRender} placeholder='UserBio'></input>
-<button onClick={showInfo}>Submit</button>
-
-</form>
-
-
-    </>
+    {returnInputsOrTexts()}
+  </>
   )
 }
 
